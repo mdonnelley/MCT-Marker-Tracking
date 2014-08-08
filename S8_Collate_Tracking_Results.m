b70bdaad8df40d04e15b309948ec479fe92a1491
mdonnelley <martin.donnelley@gmail.com>
2014-08-08 11:58:52 +0930

    S8_2012A_Collate_1

--- S8_Collate_Tracking_Results.m
+++ S8_Collate_Tracking_Results.m
@@ -1,51 +1,37 @@
-% Script to collate data about MCT rates in each animal
+% Script to collate data about mean MCT rate in each animal
 
-PathName = 'I:/SPring-8/2011 B/20XU/MCT/Images/Processed/MCT Rate Calculation/R03/';
-cols = 11:13;
+PathName = 'I:/SPring-8/2012 A/20XU/MCT/Images/Processed/MCT Rate Calculation/R01/';
+cols = 11:12;
 
-B = 'MCT Rate Calculation 2013-Apr-23 12-05-32 MD - Baseline.xls';
-C = 'MCT Rate Calculation 2013-Apr-23 13-01-13 MD - Control.xls';
-T = 'MCT Rate Calculation 2013-Apr-29 09-43-48 MD - Treatment.xls';
+B = 'MCT Rate Calculation 2013-Jul-29 10-52-28 MD Baseline.xls';
+R = 'MCT Rate Calculation 2013-Jul-29 13-44-21 MD Repeat.xls';
 
 [B_status,B_sheets] = xlsfinfo([PathName,B]);
-[C_status,C_sheets] = xlsfinfo([PathName,C]);
-[T_status,T_sheets] = xlsfinfo([PathName,T]);
-
-B_rows = [1,251];
-C_rows = 501:250:2750;
-T_rows = 1001:500:5500;
+[R_status,R_sheets] = xlsfinfo([PathName,R]);
 
 [animals,B_IX] = sort(B_sheets);
 
 for s = 1:length(B_sheets),
     
-    IX = find(strcmp(animals,B_sheets(s)));
+    IX = find(B_IX == s);
     data = xlsread([PathName,B],s);
-    stats(1:2,:,IX) = data(B_rows,cols);
+    particles = max(data((data(:,4) > 0) & (data(:,5) > 0),2));
+    if(isempty(particles)), particles=0; end
+    B_stats(IX,:) = [data(1,cols),particles];
     
 end
 
-for s = 1:length(C_sheets),
+[animals,R_IX] = sort(R_sheets);
+
+for s = 1:length(R_sheets),
     
-    IX = find(strcmp(animals,C_sheets(s)));
-    data = xlsread([PathName,C],s);
-    stats(3:11,:,IX) = data(C_rows,cols);
+    IX = find(R_IX == s);
+    data = xlsread([PathName,R],s);
+    particles = max(data((data(:,4) > 0) & (data(:,5) > 0),2));
+    if(isempty(particles)), particles=0; end
+    R_stats(IX,:) = [data(1,cols),particles];
     
 end
 
-for s = 1:length(T_sheets),
-    
-    IX = find(strcmp(animals,T_sheets(s)));
-    data = xlsread([PathName,T],s);
-    stats(3:11,:,IX) = data(T_rows,cols);
-    
-end
-
-stats(isnan(stats)) = 0;
-average = squeeze(stats(:,1,:));
-stdev = squeeze(stats(:,2,:));
-extra = squeeze(stats(:,3,:));
-
-average = average(:,[1 3 10 11 12 2 5 6 7 4 8 9]);
-stdev = stdev(:,[1 3 10 11 12 2 5 6 7 4 8 9]);
-extra = extra(:,[1 3 10 11 12 2 5 6 7 4 8 9]);
+output = [B_stats,R_stats];
+output(isnan(output))=0)
