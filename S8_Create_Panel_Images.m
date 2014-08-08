f9288d3565cae71a46f6b96d5bae53e20b112118
mdonnelley <martin.donnelley@gmail.com>
2014-08-08 12:04:05 +0930

    S8_2012B_Panel_1

--- S8_Create_Panel_Images.m
+++ S8_Create_Panel_Images.m
@@ -1,49 +1,80 @@
-if(isunix), datapath = '/media/RAID0/Data/SPring-8/';
+% Script to mark timestamp and treatment delivery on each image
+
+warning('off','vision:transition:usesOldCoordinates')
+
+if(isunix), datapath = '/data/RAID0/exports/processing/SPring-8/';
 elseif(ispc), datapath = 'P:/SPring-8/'; end
 
-% Set read and write paths
-read = [datapath,'2012 A/20XU/MCT/Images/FD Corrected/'];
-write = [datapath,'2012 A/20XU/MCT/Images/Processed/Vertical/'];
+experiment.read = '2012 B/MCT/Images/FD Corrected/';
+experiment.write = '2012 B/MCT/Images/Processed/Movie Frames/';
+experiment.filelist = '2012 B/MCT/Images/2012B Data.csv';
+experiment.runlist = [1,4:9,11:16];
 
-% Determine the animals in the read path
-animals = dir(read);
+FAD_IMAGESET_L = 'Low/';
+FAD_FILENAME_L = 'fad_';
+FAD_FILETYPE_L = '.jpg';
 
-% gap = uint8(255*ones(2160,50));
-gap = uint8(255*ones(50,2560));
+info = ReadS8Data([datapath,experiment.filelist]);
 
-for i = 3:length(animals),
+group = {...
+    'Hypertonic Saline',... % S8_12B_XU_34
+    'N/A',...
+    'N/A',...
+    'Hypertonic Saline',... % S8_12B_XU_37
+    'Hypertonic Saline',... % S8_12B_XU_38
+    'Hypertonic Saline',... % S8_12B_XU_39
+    'Hypertonic Saline',... % S8_12B_XU_40
+    'Hypertonic Saline',... % S8_12B_XU_41
+    'Mannitol',... % S8_12B_XU_42
+    'N/A',...
+    'Mannitol',... % S8_12B_XU_43
+    'Mannitol',... % S8_12B_XU_44
+    'Mannitol',... % S8_12B_XU_45
+    'Mannitol',... % S8_12B_XU_46
+    'Mannitol',... % S8_12B_XU_48
+    'Mannitol',... % S8_12B_XU_49
+    };
+
+% For each animal
+for m = experiment.runlist,
     
-    % Set the current read and write directory
-    current_read = [read,animals(i).name,'/Low/'];
-    current_write = [write,animals(i).name,'/'];
+    % Set the current write directory
+    current_write = [datapath,experiment.write,info.image{m}]
     
     if(~exist(current_write)), mkdir(current_write), end
     
-    R01 = dir([current_read,'*R01*.jpg']);
-    R03 = dir([current_read,'*R03*.jpg']);
+    count = 1;
     
-    % Create each composite frame
-    for frame = 1:110,
+    % For each frame
+    for frame = 10:info.imagegoto(m),
         
-        % Load the right frame
-        r = imread([current_read,R03(frame).name]);
+        % Read the current image
+        current = sprintf('%s%s%s%s%s%s%.4d%s',datapath,experiment.read,info.image{m},FAD_IMAGESET_L,info.imagestart{m},FAD_FILENAME_L,frame,FAD_FILETYPE_L);
+        input = imread(current);
         
-        % Load the left frame
-        if((frame >= 1)&(frame <= 12)),
-            l = imread([current_read,R01(frame).name]);
-        
-        elseif((frame >= 55)&(frame <= 66)),
-            l = imread([current_read,R01(frame-42).name]);
-        else
-            l = zeros(size(r));
-        end
+        % Determine the time information
+        seconds = (frame - 120) / 2;
+        min = fix(seconds/60);
+        sec = abs(seconds) - min * 60;
 
-        % Create the panel image
-%         output = [l,gap,r];
-        output = [l;gap;r];
+        % Add the text
+        if seconds <= 0
+            htxtins = vision.TextInserter(sprintf('%.2d:%04.1f [Baseline]', min, sec));
+        elseif (seconds > 0) & (seconds < 60)
+            htxtins = vision.TextInserter(sprintf('%.2d:%04.1f [%s Aerosol ON]', min, sec, group{m}));
+        else
+            htxtins = vision.TextInserter(sprintf('%.2d:%04.1f [Post %s Rx]', min, sec, group{m}));
+        end
+        htxtins.Color = [255, 255, 255]; % [red, green, blue]
+        htxtins.FontSize = 72;
+        htxtins.Location = [20 20]; % [x y]
+        output = step(htxtins, input);
         
-        % Write the image
-        imwrite(output,sprintf('%s%s-%0.2d.jpg',current_write,animals(i).name,frame));
+        imwrite(output,sprintf('%sFrame %0.4d.jpg',current_write,count));
+%         figure(1),imshow(output);
+
+        count = count + 1;
         
     end
+    
 end
