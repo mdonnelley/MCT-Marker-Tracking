129070767cedd707fb07a7c0b01d5b1afbd2bfa4
mdonnelley <martin.donnelley@gmail.com>
2014-08-08 12:00:22 +0930

    S8_2012A_Collate_3

--- S8_Collate_Tracking_Results.m
+++ S8_Collate_Tracking_Results.m
@@ -1,25 +1,23 @@
-% Script to collate data about number of particles in each animal
+% Script to collate data about MCT rate of all particles in each animal
 
 PathName = 'I:/SPring-8/2012 A/20XU/MCT/Images/Processed/MCT Rate Calculation/R01/';
+cols = 10;
 
-baseline_IX = 1:24;
-three_IX = 17:24;
-nine_IX = 9:16;
-twentyfive_IX = 1:8;
-
-B = 'Particle Count 2013-Jul-30 10-34-23 Baseline.xls';
-R = 'Particle Count 2013-Jul-30 11-59-36 Repeat';
+B = 'MCT Rate Calculation 2013-Jul-29 10-52-28 MD Baseline.xls';
+R = 'MCT Rate Calculation 2013-Jul-29 13-44-21 MD Repeat.xls';
 
 [B_status,B_sheets] = xlsfinfo([PathName,B]);
 [R_status,R_sheets] = xlsfinfo([PathName,R]);
 
-[animals,B_IX] = sort(B_sheets);
+baseline = [];
+three = [];
+nine = [];
+twentyfive = [];
 
 for s = 1:length(B_sheets),
     
-    IX = find(strcmp(animals,B_sheets(s)));
     data = xlsread([PathName,B],s);
-    B_counts(IX) = size(data,1);
+    baseline = [baseline;data(data(:,cols) > 0, cols)];
     
 end
 
@@ -27,13 +25,14 @@ end
 
 for s = 1:length(R_sheets),
     
-    IX = find(strcmp(animals,R_sheets(s)));
+    IX = find(R_IX == s);
     data = xlsread([PathName,R],s);
-    R_counts(IX) = size(data,1);
+    if(IX <= 8),
+        nine = [nine;data(data(:,cols) > 0, cols)];
+    elseif(IX >= 9 && IX <= 16),
+        twentyfive = [twentyfive;data(data(:,cols) > 0, cols)];
+    elseif(IX >= 17),
+        three = [three;data(data(:,cols) > 0, cols)];
+    end
     
 end
-
-B_counts(baseline_IX)'
-R_counts(three_IX)'
-R_counts(nine_IX)'
-R_counts(twentyfive_IX)'
