f7dfad4bbf6d5d538542b3d5ccfba1c81df98357
mdonnelley <martin.donnelley@gmail.com>
2014-08-08 12:04:31 +0930

    S8_2012B_Panel_2

--- S8_Create_Panel_Images.m
+++ S8_Create_Panel_Images.m
@@ -1,79 +1,32 @@
-% Script to mark timestamp and treatment delivery on each image
-
-warning('off','vision:transition:usesOldCoordinates')
+% Script to make 2 panel image sequences comparing Mannitol and HS
 
 if(isunix), datapath = '/data/RAID0/exports/processing/SPring-8/';
 elseif(ispc), datapath = 'P:/SPring-8/'; end
 
-experiment.read = '2012 B/MCT/Images/FD Corrected/';
-experiment.write = '2012 B/MCT/Images/Processed/Movie Frames/';
-experiment.filelist = '2012 B/MCT/Images/2012B Data.csv';
-experiment.runlist = [1,4:9,11:16];
+% Set read and write paths
+read = [datapath,'2012 B/MCT/Images/Processed/Movie Frames/'];
+write = [datapath,'2012 B/MCT/Images/Processed/Panels/'];
 
-FAD_IMAGESET_L = 'Low/';
-FAD_FILENAME_L = 'fad_';
-FAD_FILETYPE_L = '.jpg';
+panels = [34,42;37,43;38,44;39,45;40,46;41,48];
+gap = 20;
 
-info = ReadS8Data([datapath,experiment.filelist]);
-
-group = {...
-    'Hypertonic Saline',... % S8_12B_XU_34
-    'N/A',...
-    'N/A',...
-    'Hypertonic Saline',... % S8_12B_XU_37
-    'Hypertonic Saline',... % S8_12B_XU_38
-    'Hypertonic Saline',... % S8_12B_XU_39
-    'Hypertonic Saline',... % S8_12B_XU_40
-    'Hypertonic Saline',... % S8_12B_XU_41
-    'Mannitol',... % S8_12B_XU_42
-    'N/A',...
-    'Mannitol',... % S8_12B_XU_43
-    'Mannitol',... % S8_12B_XU_44
-    'Mannitol',... % S8_12B_XU_45
-    'Mannitol',... % S8_12B_XU_46
-    'Mannitol',... % S8_12B_XU_48
-    'Mannitol',... % S8_12B_XU_49
-    };
-
-% For each animal
-for m = experiment.runlist,
+for panel = 2:size(panels,1),
     
-    % Set the current write directory
-    current_write = [datapath,experiment.write,info.image{m}]
-    
-    if(~exist(current_write)), mkdir(current_write), end
-    
-    count = 1;
-    
-    % For each frame
-    for frame = 10:info.imagegoto(m),
+    for frame = 1:2400,
+ 
+        M = sprintf('%sS8_12B_XU_%d/Frame %0.4d.jpg',read,panels(panel,1),frame);
+        HS = sprintf('%sS8_12B_XU_%d/Frame %0.4d.jpg',read,panels(panel,2),frame);
         
-        % Read the current image
-        current = sprintf('%s%s%s%s%s%s%.4d%s',datapath,experiment.read,info.image{m},FAD_IMAGESET_L,info.imagestart{m},FAD_FILENAME_L,frame,FAD_FILETYPE_L);
-        input = imread(current);
+        M = imread(M);
+        HS = imread(HS);
         
-        % Determine the time information
-        seconds = (frame - 120) / 2;
-        min = fix(seconds/60);
-        sec = abs(seconds) - min * 60;
-
-        % Add the text
-        if seconds <= 0
-            htxtins = vision.TextInserter(sprintf('%.2d:%04.1f [Baseline]', min, sec));
-        elseif (seconds > 0) & (seconds < 60)
-            htxtins = vision.TextInserter(sprintf('%.2d:%04.1f [%s Aerosol ON]', min, sec, group{m}));
-        else
-            htxtins = vision.TextInserter(sprintf('%.2d:%04.1f [Post %s Rx]', min, sec, group{m}));
-        end
-        htxtins.Color = [255, 255, 255]; % [red, green, blue]
-        htxtins.FontSize = 72;
-        htxtins.Location = [20 20]; % [x y]
-        output = step(htxtins, input);
+        output = [M,zeros(size(M,1),gap),HS];
+        output = imresize(output,0.25);
         
-        imwrite(output,sprintf('%sFrame %0.4d.jpg',current_write,count));
-%         figure(1),imshow(output);
-
-        count = count + 1;
+        outFolder = sprintf('%s%d and %d/',write,panels(panel,1),panels(panel,2));
+        if(~exist(outFolder)), mkdir(outFolder), end
+        outFile = sprintf('Frame %0.4d.jpg',frame);
+        imwrite(output,[outFolder,outFile]);
         
     end
     
