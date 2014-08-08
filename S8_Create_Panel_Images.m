94db2515c30e697300330b98a0235b50383c63b2
mdonnelley <martin.donnelley@gmail.com>
2014-08-08 12:01:54 +0930

    S8_2012A_Panel_1

--- S8_Create_Panel_Images.m
+++ S8_Create_Panel_Images.m
@@ -1,22 +1,49 @@
-% Script to make 3 panel image comparing NT, NS and HS
-
-if(isunix), datapath = '/data/RAID0/exports/processing/SPring-8/';
+if(isunix), datapath = '/media/RAID0/Data/SPring-8/';
 elseif(ispc), datapath = 'P:/SPring-8/'; end
 
 % Set read and write paths
-read = [datapath,'2011 B/20XU/MCT/Images/Processed/Movie Frames/'];
-write = [datapath,'2011 B/20XU/MCT/Images/Processed/Panels/Three/'];
+read = [datapath,'2012 A/20XU/MCT/Images/FD Corrected/'];
+write = [datapath,'2012 A/20XU/MCT/Images/Processed/Vertical/'];
 
-for frame = 1:708,
+% Determine the animals in the read path
+animals = dir(read);
+
+% gap = uint8(255*ones(2160,50));
+gap = uint8(255*ones(50,2560));
+
+for i = 3:length(animals),
     
-    NT = imread(sprintf('%sMX17/Frame %0.3d.jpg',read,frame));
-    NS = imread(sprintf('%sMX09/Frame %0.3d.jpg',read,frame));
-    HS = imread(sprintf('%sMX15/Frame %0.3d.jpg',read,frame));
+    % Set the current read and write directory
+    current_read = [read,animals(i).name,'/Low/'];
+    current_write = [write,animals(i).name,'/'];
     
-    output = [NT,NS,HS];
+    if(~exist(current_write)), mkdir(current_write), end
     
-    output = imresize(output,0.3);
-   
-    imwrite(output,sprintf('%sFrame %0.3d.jpg',write,frame));
+    R01 = dir([current_read,'*R01*.jpg']);
+    R03 = dir([current_read,'*R03*.jpg']);
     
+    % Create each composite frame
+    for frame = 1:110,
+        
+        % Load the right frame
+        r = imread([current_read,R03(frame).name]);
+        
+        % Load the left frame
+        if((frame >= 1)&(frame <= 12)),
+            l = imread([current_read,R01(frame).name]);
+        
+        elseif((frame >= 55)&(frame <= 66)),
+            l = imread([current_read,R01(frame-42).name]);
+        else
+            l = zeros(size(r));
+        end
+
+        % Create the panel image
+%         output = [l,gap,r];
+        output = [l;gap;r];
+        
+        % Write the image
+        imwrite(output,sprintf('%s%s-%0.2d.jpg',current_write,animals(i).name,frame));
+        
+    end
 end
