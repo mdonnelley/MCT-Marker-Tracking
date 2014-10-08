530d33feadcf71be1e2d3ebeb3a39ca2ebc01d0f
mdonnelley <martin.donnelley@gmail.com>
2014-10-08 11:53:48 +1030

    Change from script to function

--- Condense_XLS_Format.m
+++ Condense_XLS_Format.m
@@ -1,40 +1,49 @@
-[readname,pathname] = uigetfile({'*.xls','*.xlsx'});
+function Condense_XLS_Format(XLS)
 
 % Get the sheet names
-[status,sheets] = xlsfinfo([pathname,readname]);
+[status,sheets] = xlsfinfo(XLS);
 
 % Create the output filename
-writename = [readname(1:length(readname)-4), ' - Condensed', readname(length(readname)-3:length(readname))]
+writename = [XLS(1:length(XLS)-4), ' - Condensed', XLS(length(XLS)-3:length(XLS))];
 
-for i = 4:length(sheets),
+for s = length(sheets)-1,
     
-    sheets{i}
-    
-    % Load the sheet
-    data = xlsread([pathname,readname],sheets{i});
-    
-    % Remove rows that start with a zero in col 1
-    data(data(:,1)==0,:) = [];
-    
-    % Remove rows that contain NaN in col 4
-    data(isnan(data(:,4)),:) = [];
-    
-    if(isempty(data)), 
+    if ~strcmp(sheets{s},'Sheet1') & ~strcmp(sheets{s},'Sheet2') & ~strcmp(sheets{s},'Sheet3'),
         
-        data = NaN; 
-    
-    else
+        % Load the sheet
+        data = xlsread(XLS,sheets{s});
         
-        % Calculate mean and standard deviation data for each timepoint
-        [C,ia,ic] = unique(data(:,1));
-        for j = 1:length(C),
-            data(ia(j),11) = nanmean(data(data(:,1) == C(j),10));
-            data(ia(j),12) = nanstd(data(data(:,1) == C(j),10));
+        if isempty(data),
+            
+            data = NaN;
+            
+        else
+            
+            % Remove rows that start with a zero in col 1
+            data(data(:,1)==0,:) = [];
+            
+            % Remove rows that contain NaN in col 4
+            data(isnan(data(:,4)),:) = [];
+            
+            % Remove zeros from stats columns
+            data(data(:,11) == 0,11) = NaN;
+            data(data(:,12) == 0,12) = NaN;
+            
+            % Remove median value if present
+            if size(data,2) == 13, data(:,13) = []; end
+            
+            % Calculate mean and standard deviation data for each timepoint
+            [C,ia,ic] = unique(data(:,1));
+            for j = 1:length(C),
+                data(ia(j),11) = nanmean(data(data(:,1) == C(j),10));
+                data(ia(j),12) = nanstd(data(data(:,1) == C(j),10));
+            end
+            
         end
         
+        % Write the data out
+        xlswrite(writename,data,sheets{s});
+        
     end
-    
-    % Write the data out
-    xlswrite([pathname,writename],data,sheets{i});
     
 end
