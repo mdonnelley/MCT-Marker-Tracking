561e003afce535ba5eaa448bb398772d84c9ed97
mdonnelley <martin.donnelley@gmail.com>
2014-10-22 13:39:47 +1030

    Add waitbar, clear stats array, exclude stationary
    Waitbars added where appropriate. Stats array wasn't cleared previously
    so data remained in the array between subjects. Exclude stationary
    particles and rapidly moving particles.

--- S8_Collate_Tracking_Results.m
+++ S8_Collate_Tracking_Results.m
@@ -5,6 +5,8 @@
 
 function S8_Collate_Tracking_Results(XLS)
 
+w = waitbar(0,'Reading MAT and XLS data');
+
 % Set the base pathname for the current machine
 setbasepath;
 
@@ -29,17 +31,23 @@ number = NaN(length(expt.info.image),length(expt.tracking.times));
 
 for s = 1:length(sheets)
     
+    waitbar(s/length(sheets),w,['Reading sheet: ',sheets{s}(1:length(sheets{s})-1)]);
+    
     if ~strcmp(sheets{s},'Sheet1') & ~strcmp(sheets{s},'Sheet2') & ~strcmp(sheets{s},'Sheet3') & ~strcmp(sheets{s},'Mean') & ~strcmp(sheets{s},'SD') & ~strcmp(sheets{s},'Number'),
         
         % Read each XLS sheet
-        data = xlsread(XLS,sheets{s},'','basic');
+        data = xlsread(XLS,sheets{s});
         
         if ~isempty(data)
             
+            % Exclude stationary particles
+            data(data(:,10) == 0,10) = NaN;
+            
             % Exclude rapidly moving particles
-            if isfield(expt.tracking,'maxrate'), data(data(:,10) > expt.tracking.maxrate,10) = 0; end
+            if isfield(expt.tracking,'maxrate'), data(data(:,10) > expt.tracking.maxrate,10) = NaN; end
             
             % Get the summary stats info
+            stats = [];
             [C,ia,ic] = unique(data(:,1));
             for i = 1:length(C),
                 stats(1,i) = C(i);
@@ -71,19 +79,24 @@ for s = 1:length(sheets)
 end
 
 % Write the mean data back to the XLS sheet
+waitbar(0,w,'Writing sheet: Mean');
 xlswrite(XLS,expt.info.imagestart,'Mean','A2');
 xlswrite(XLS,groups,'Mean','B2');
 xlswrite(XLS,sort(expt.tracking.times),'Mean','C1');
 xlswrite(XLS,average,'Mean','C2');
 
 % Write the standard deviation data back to the XLS sheet
+waitbar(0.33,w,'Writing sheet: SD');
 xlswrite(XLS,expt.info.imagestart,'SD','A2');
 xlswrite(XLS,groups,'SD','B2');
 xlswrite(XLS,sort(expt.tracking.times),'SD','C1');
 xlswrite(XLS,SD,'SD','C2');
 
 % Write the number of particles tracked back to the XLS sheet
+waitbar(0.66,w,'Writing sheet: Number');
 xlswrite(XLS,expt.info.imagestart,'Number','A2');
 xlswrite(XLS,groups,'Number','B2');
 xlswrite(XLS,sort(expt.tracking.times),'Number','C1');
 xlswrite(XLS,number,'Number','C2');
+
+close(w)
