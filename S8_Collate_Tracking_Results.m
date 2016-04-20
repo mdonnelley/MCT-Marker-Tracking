80db03181d6b6622801872ff1153e56ab55cdc48
mdonnelley <martin.donnelley@gmail.com>
2016-04-20 12:33:56 +0930

    Bug fixes
    Included default values. Changed allocated arrays from NaN to zero to
    work better with GraphPad Prism (since no tracked particles = zero
    average MCT rate). Added minparticles parameter.

--- S8_Collate_Tracking_Results.m
+++ S8_Collate_Tracking_Results.m
@@ -25,11 +25,17 @@ end
 % Get the sheet names
 [status,sheets] = xlsfinfo(XLS);
 
+% Set default values
+if isfield(expt.tracking(tracked),'bins') & ~isempty(expt.tracking(tracked).bins), bins = expt.tracking(tracked).bins; else bins = 0:0.05:5; end
+if isfield(expt.tracking(tracked),'maxrate') & ~isempty(expt.tracking(tracked).maxrate), maxrate = expt.tracking(tracked).maxrate; else maxrate = Inf; end
+if isfield(expt.tracking(tracked),'minrate') & ~isempty(expt.tracking(tracked).minrate), minrate = expt.tracking(tracked).minrate; else minrate = 0; end
+if isfield(expt.tracking(tracked),'minparticles') & ~isempty(expt.tracking(tracked).minparticles), minparticles = expt.tracking(tracked).minparticles; else minparticles = 0; end
+
 % Pre-allocate the arrays
-average = NaN(length(expt.info.image),length(expt.tracking(tracked).times));
-SD = NaN(length(expt.info.image),length(expt.tracking(tracked).times));
-number = NaN(length(expt.info.image),length(expt.tracking(tracked).times));
-histogram = zeros(length(expt.tracking(tracked).bins),length(expt.tracking(tracked).times),length(grouplist));
+average = zeros(length(expt.info.image),length(expt.tracking(tracked).times));
+SD = zeros(length(expt.info.image),length(expt.tracking(tracked).times));
+number = zeros(length(expt.info.image),length(expt.tracking(tracked).times));
+histogram = zeros(length(bins),length(expt.tracking(tracked).times),length(grouplist));
 
 for s = 1:length(sheets)
     
@@ -43,26 +49,31 @@ for s = 1:length(sheets)
         if ~isempty(data)
             
             % Exclude stationary particles and those moving outside max/min rates
-            data(data(:,10) == 0,10) = NaN;
-            if isfield(expt.tracking(tracked),'maxrate') & ~isempty(expt.tracking(tracked).maxrate), data(data(:,10) >= expt.tracking(tracked).maxrate,10) = NaN; end
-            if isfield(expt.tracking(tracked),'minrate') & ~isempty(expt.tracking(tracked).minrate), data(data(:,10) <= expt.tracking(tracked).minrate,10) = NaN; end
+            data(data(:,10) >= maxrate,:) = NaN;
+            data(data(:,10) <= minrate,:) = NaN;
             
             % Get the summary stats info for all particles at each timepoint
             stats = [];
-            h2D = zeros(length(expt.tracking(tracked).bins),length(expt.tracking(tracked).times));
+            h2D = zeros(length(bins),length(expt.tracking(tracked).times));
             [C,ia,ic] = unique(data(:,1));
             for i = 1:length(C),
-                
-                stats(1,i) = C(i);
-                stats(2,i) = nanmean(data(ic == i,10));
-                stats(3,i) = nanstd(data(ic == i,10));
-                stats(4,i) = max(data(ic == i,2));
-                
-                % Calculate the histogram data based on the mean MCT rate of each particle
-                for j = 1:max(data(ic == i,2)),
-                    h1D = hist(nanmean(data((ic == i) & (data(:,2) == j),10)),expt.tracking(tracked).bins)';
-                    timepoint = find(expt.tracking(tracked).times == C(i));
-                    h2D(:,timepoint) = h2D(:,timepoint) + h1D;
+                    
+                % Exclude if there are an insufficient number of particles
+                numparticles = max(data(ic == i,2));
+                if(numparticles >= minparticles),
+                    
+                    stats(1,i) = C(i);
+                    stats(2,i) = nanmean(data(ic == i,10));
+                    stats(3,i) = nanstd(data(ic == i,10));
+                    stats(4,i) = length(unique(data(ic == i,2)));
+                    
+                    % Calculate the histogram data based on the mean MCT rate of each particle
+                    for j = 1:numparticles,
+                        h1D = hist(nanmean(data((ic == i) & (data(:,2) == j),10)),bins)';
+                        timepoint = find(expt.tracking(tracked).times == C(i));
+                        h2D(:,timepoint) = h2D(:,timepoint) + h1D;
+                    end
+                    
                 end
                 
             end
@@ -70,7 +81,7 @@ for s = 1:length(sheets)
             % Save histogram in the XLS sheet
             xlswrite(XLS,NaN(200,200),sheets{s},'L1');
             xlswrite(XLS,sort(expt.tracking(tracked).times),sheets{s},'M1');
-            xlswrite(XLS,expt.tracking(tracked).bins',sheets{s},'L2');
+            xlswrite(XLS,bins',sheets{s},'L2');
             xlswrite(XLS,h2D,sheets{s},'M2');
             
             % Get the row number
@@ -125,9 +136,9 @@ xlswrite(XLS,number,'Number','C2');
 % Write the histogram data back to the XLS sheet
 waitbar(0.75,w,'Writing sheet: Histogram');
 xlswrite(XLS,NaN(200,200),'Histogram','A1');
-histogram = reshape(histogram,length(expt.tracking(tracked).bins),[]);
+histogram = reshape(histogram,length(bins),[]);
 xlswrite(XLS,repmat(sort(expt.tracking(tracked).times),[1 size(groups,2)]),'Histogram','B1');
-xlswrite(XLS,expt.tracking(tracked).bins','Histogram','A2');
+xlswrite(XLS,bins','Histogram','A2');
 xlswrite(XLS,histogram,'Histogram','B2');
 
 close(w)
