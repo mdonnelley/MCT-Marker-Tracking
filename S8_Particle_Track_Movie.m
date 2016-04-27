0ff01ab5135d58da36e8defd198bc0a66b939661
mdonnelley <martin.donnelley@gmail.com>
2016-04-27 12:03:20 +0930

    Bug fixes and change times to frames
    Changed times to frames to match metadata.  If the image does not exist
    the program now creates a zeros image. Image information is stamped on
    top left regardless of whether coordinates are present.

--- S8_Particle_Track_Movie.m
+++ S8_Particle_Track_Movie.m
@@ -8,15 +8,16 @@ function S8_Particle_Track_Movie(MAT)
 % Set the base pathname for the current machine
 setbasepath;
 
-SCALE = 0.5;
+% Set image scaling
+SCALE = 1;
 
 w = waitbar(0,'Reading MAT and XLS data');
 load(MAT);
 XLS = [MAT(1:length(MAT)-4),'.xls'];
 
 % Set the start timepoints (in frames)
-starttimes = expt.tracking(tracked).blockimages * expt.tracking(tracked).times + expt.tracking(tracked).startframe;
-starttimes = sort(starttimes);
+startframes = expt.tracking(tracked).blockimages * expt.tracking(tracked).blocks + expt.tracking(tracked).startframe;
+startframes = sort(startframes);
 
 % Get the sheet names
 [status,sheets] = xlsfinfo(XLS);
@@ -45,18 +46,18 @@ for s = 1:length(sheets),
             mkdir(current_dir);
             
             % Repeat for each timepoint
-            for t = 1:length(starttimes),
+            for t = 1:length(startframes),
                 
                 % Load each of the images at that timepoint
-                waitbar(0,w,['Sheet ',num2str(s),' of ',num2str(length(sheets)),': Timepoint ',num2str(t),' of ',num2str(length(starttimes))]);
+                waitbar(0,w,['Sheet ',num2str(s),' of ',num2str(length(sheets)),': Timepoint ',num2str(t),' of ',num2str(length(startframes))]);
                 for i = 1:expt.tracking(tracked).frames,
                     
                     waitbar(i/expt.tracking(tracked).frames,w);
                     
                     % Calculate the framenumber
-                    framenumber(i) = starttimes(t) + (i - 1) * expt.tracking(tracked).gap + 1;
+                    framenumber(i) = startframes(t) + (i - 1) * expt.tracking(tracked).gap + 1;
                     
-                    % Determine the imagename
+                    % Determine the input imagename and read the image
                     imagename = sprintf('%s%s%s%s%s%.4d%s',...
                         [basepath,expt.fad.corrected],...
                         expt.info.image{m},...
@@ -64,39 +65,48 @@ for s = 1:length(sheets),
                         expt.info.imagestart{m},...
                         expt.fad.FAD_file_low,...
                         framenumber(i),...
-                        expt.fad.FAD_type_low)
-                    
+                        expt.fad.FAD_type_low);
+
                     if exist(imagename),
-                        
-                        % Load and prep the image data
                         im = imread(imagename);
-                        im = repmat(im,[1 1 3]);
-                        im = imresize(im,SCALE);
+                    else
+                        im = uint8(zeros(expt.tracking(tracked).imsize));
+                    end
+
+                    % Prep the image data
+                    im = repmat(im,[1 1 3]);
+                    im = imresize(im,SCALE);
+
+                    % Add information text to the image
+                    text = sprintf('%s%s%.4d (t = %d min)',...
+                        expt.info.imagestart{m},...
+                        expt.fad.FAD_file_low,...
+                        framenumber(i),...
+                        expt.tracking(tracked).times(t));
+                    textInserter = vision.TextInserter(text, 'Color', [0 0 255], 'FontSize', 36, 'Location', [50 50]);
+                    im = step(textInserter, im);
+                    
+                    % Find frame numbers in data that match the current frame
+                    coordinates = data((data(:,3) == framenumber(i)) & isfinite(data(:,4)),4:5);
+                    
+                    if ~isempty(coordinates)
                         
-                        % Find frame numbers in data that match the current frame
-                        coordinates = data((data(:,3) == framenumber(i)) & isfinite(data(:,4)),4:5);
-                        
-                        if ~isempty(coordinates)
-                            
-                            % Add the O markers showing particle positions
-                            markerInserter = vision.MarkerInserter('Shape','Circle','Size',8*SCALE,'Fill',1,'FillColor','Custom','CustomFillColor',[0 0 255],'Opacity',0.5);
-                            marker = SCALE*int32(coordinates(1:size(coordinates,1),:));
-                            im = step(markerInserter, im, marker);
-                            
-                        end
-                        
-                        % Determine the imagename
-                        imagename = sprintf('%s%s%.4d%s',...
-                            current_dir,...
-                            expt.info.imagestart{m},...
-                            framecounter,...
-                            expt.fad.FAD_type_low);
-                        
-                        % Write the image
-                        imwrite(im,imagename);
-                        framecounter = framecounter + 1;
+                        % Add the O markers showing particle positions (30 for large and 18 for small)
+                        markerInserter = vision.MarkerInserter('Shape','Circle','Size',24*SCALE,'Fill',1,'FillColor','Custom','CustomFillColor',[255 0 0],'Opacity',0.2);
+                        marker = SCALE*int32(coordinates(1:size(coordinates,1),:));
+                        im = step(markerInserter, im, marker);
                         
                     end
+                    
+                    % Determine the output imagename and write the image
+                    imagename = sprintf('%s%s%.4d%s',...
+                        current_dir,...
+                        expt.info.imagestart{m},...
+                        framecounter,...
+                        expt.fad.FAD_type_low);
+
+                    imwrite(im,imagename);
+                    framecounter = framecounter + 1;
                     
                 end
             end
