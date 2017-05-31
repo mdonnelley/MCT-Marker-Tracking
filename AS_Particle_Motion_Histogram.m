f5acffd949b7d51a6698f743bdd2ad519843eda9
mdonnelley <martin.donnelley@gmail.com>
2017-05-31 11:02:30 +0930

    Bug fixes
    Add new example code, ability to use saved colormap with zero = white,
    and modifications to ndhist for MuCLS data.

--- AS_Particle_Motion_Histogram.m
+++ AS_Particle_Motion_Histogram.m
@@ -13,11 +13,19 @@ function AS_Particle_Motion_Histogram(expt, data, m)
 % load(MAT)
 % Baseline{1} = cell2mat(data(expt.group.Baseline));
 % AS_Particle_Motion_Histogram(expt, Baseline, 1);
+% 
+% % Example of MuCLS data analysis
+% MAT = 'I:\MuCLS\2017-1\Processed\Detected\Tracking 2017-May-15 08-38-29.mat';
+% load(MAT)
+% tube{4,:} = data{4}(data{4}(:,5) <= 640,:);
+% trachea{4,:} = data{4}(data{4}(:,5) > 640,:);
+% AS_Particle_Motion_Histogram(expt, data, 4);
 
 setbasepath;
 
 fig = figure(1);
 set (fig, 'Units', 'normalized', 'Position', [0,0,1,1]);
+load('MyColormaps','mycmap')
 
 if isempty(data{m})
     
@@ -33,11 +41,18 @@ else
     dy = dy * expt.tracking.pixelsize ./ data{m}(:,9);
     
     % Perform for all timepoints pooled together
-    ndhist(dx,-dy,'bins',2,'filter','axis',[-4 4 -4 4]);
-    title(['Instantaneous tracking particle velocity (mm/min) for ',expt.info.imagestart{m}], 'Interpreter', 'none')
-    axis equal
-    axes0
+    N = ndhist(dx,-dy,'bins',3,'filter','axis',[-4 4 -4 4]);
     
+    % NOTE: Added extra lines to ndhist for MuCLS data. Remove when finished:
+    %371:    binWidthX = 0.075
+    %372:    binWidthY = 0.075
+    %451:    N(1,1)=25;
+    
+    title(['Instantaneous tracking particle velocity (mm/min) for ',expt.info.imagestart{m}], 'Interpreter', 'none')
+    axes0
+    axis equal
+    colormap(fig,mycmap)
+
 %     % Perform for each timepoint individually
 %     for i = 0:9,
 %         
@@ -46,16 +61,17 @@ else
 %         ndhist(dx(find(data{m}(:,1) == i)),-dy(find(data{m}(:,1) == i)),'bins',2,'filter','axis',[-5 5 -5 5]);
 %         title(['t = ',num2str(i), ' minutes'])
 %         %         title(['Instantaneous tracking particle velocity (mm/min) for ',expt.info.imagestart{m}], 'Interpreter', 'none')
-%         axis equal
 %         axes0
+%         axis equal
+%         colormap(ax,mycmap)
 %         
 %     end
-    
+
+    % Write the image
     outfile = [basepath,...
         expt.tracking.MCT,...
         expt.info.imagestart{m},...
         'hist2d.jpg'];
-    
-    saveas(gcf,outfile);
+    saveas(fig,outfile);
     
 end
